bf0dd8e54539f3576b676f5be1fd0651bb788e4c
YuraMalinko <malinkowork@gmail.com>
2022-12-11 00:34:30 +0300

    Add properties and contructor for Board Class

--- TaskManager/Board.cs
+++ TaskManager/Board.cs
@@ -4,9 +4,18 @@
     {
         public int NumberBoard { get; private set; }
 
-        public Board(int numberBoard)
+        public List<string> IDMembers { get; private set; }
+
+        public List<string> IDAdmin { get; private set; }
+
+        public List<Task> Tasks { get; private set; }
+
+        public List<int> Key { get; private set; }
+
+        public Board(int numberBoard, string idAdmin)
         {
             NumberBoard = numberBoard;
+            IDAdmin.Add(idAdmin);
         }
     }
 }
