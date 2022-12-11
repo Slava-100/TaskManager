861d734a79e2771964f1d57b30e2071f40145362
MIX4H <m-3k@mail.ru>
2022-12-11 23:31:52 +0300

    Add constructor

--- TaskManager/User.cs
+++ TaskManager/User.cs
@@ -4,6 +4,14 @@
     {
        private DataStorage _storage;
       public  string IDUser { get; private set; }
+      public  string NameUser { get; private set; }
+      public  List<Board> BoardsForUser { get; private set; }
+        public User(string idUser, string nameUser)
+        {
+            IDUser = idUser;
+            NameUser = nameUser;
+
+        }
 
     }
 }
