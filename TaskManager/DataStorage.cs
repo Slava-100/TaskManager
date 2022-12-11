f0f9f478ad4d375bf31d1410caa44dc4e79326cb
YuraMalinko <malinkowork@gmail.com>
2022-12-12 01:40:32 +0300

    Added properties and constructor for DataStorage class

--- TaskManager/DataStorage.cs
+++ TaskManager/DataStorage.cs
@@ -2,6 +2,17 @@
 {
     public class DataStorage
     {
+        public string Path { get; set; }
 
+        public Dictionary<string,Board> Boards { get; set; }
+
+        public List <User> Users { get; set; }
+
+        public DataStorage()
+        {
+            Boards = new Dictionary<string,Board>();
+            Users = new List<User>();
+            Path = @".\DataStorage.txt";
+        }
     }
 }
