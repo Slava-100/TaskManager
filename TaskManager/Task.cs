ac07f8d5d019703222c13e32a4709d859d1a72b8
Kristina <bks1994@mail.ru>
2022-12-12 11:31:32 +0300

    2 -Filled Task Class

--- TaskManager/Task.cs
+++ TaskManager/Task.cs
@@ -2,6 +2,32 @@
 {
     public class Task
     {
-        public int _numberTask;
+        public enum StatusTask
+        {
+            UserStory,
+            Backlog,
+            InProgress,
+            Review,
+            Done
+        }
+
+        public int NumberTask { get; set; }
+
+        public string Description { get; set; }
+
+        public string IdUser { get; set; }
+
+        public string Comment { get; set; }
+
+        public StatusTask Status { get; set; }
+
+        public Task()
+        {
+        }
+
+        public Task(string description)
+        {
+            Description = description;
+        }
     }
 }
