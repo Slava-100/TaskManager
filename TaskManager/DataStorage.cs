d40904f5ce6167217b47b9cd0dd731ac4f8504a0
YuraMalinko <malinkowork@gmail.com>
2022-12-12 22:34:29 +0300

    The list on the dictionary has changed

--- TaskManager/DataStorage.cs
+++ TaskManager/DataStorage.cs
@@ -6,12 +6,12 @@
 
         public Dictionary<string,Board> Boards { get; set; }
 
-        public List <User> Users { get; set; }
+        public Dictionary <string,User> Users { get; set; }
 
         public DataStorage()
         {
             Boards = new Dictionary<string,Board>();
-            Users = new List<User>();
+            Users = new Dictionary<string, User>();
             Path = @".\DataStorage.txt";
         }
     }
