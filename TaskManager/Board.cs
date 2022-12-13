c59ba8ded14eca7d30c9d4ed4a7b37375c630dda
Kristina <bks1994@mail.ru>
2022-12-13 15:08:12 +0300

    18-AddMethodsGenerationIdForIssuesAndBoards

--- TaskManager/Board.cs
+++ TaskManager/Board.cs
@@ -10,12 +10,30 @@
 
         public List<Issue> Issues { get; private set; }
 
-        public List<int> Key { get; private set; }
+        public List<int> Keys { get; private set; }
 
         public Board(int numberBoard, string idAdmin)
         {
+            IDMembers = new List<string>();
+            IDAdmin = new List<string>();
+            Issues = new List<Issue>();
+            Keys = new List<int>();
             NumberBoard = numberBoard;
             IDAdmin.Add(idAdmin);
+        }
+
+        public int GetNextNumberIssue()
+        {
+            if (Issues.Count > 0)
+            {
+                Issue issue = Issues.LastOrDefault();
+                int issueNumber = issue.NumberIssue;
+                return issueNumber + 1;
+            }
+            else
+            {
+                return 1;
+            }
         }
     }
 }
--- TaskManager/DataStorage.cs
+++ TaskManager/DataStorage.cs
@@ -2,17 +2,34 @@
 {
     public class DataStorage
     {
+        public int NextNumberBoard { get; private set; }
+
         public string Path { get; set; }
 
-        public Dictionary<string,Board> Boards { get; set; }
+        public Dictionary<int, Board> Boards { get; set; }
 
-        public Dictionary <string,User> Users { get; set; }
+        public Dictionary<string, User> Users { get; set; }
 
         public DataStorage()
         {
-            Boards = new Dictionary<string,Board>();
+            Boards = new Dictionary<int, Board>();
             Users = new Dictionary<string, User>();
             Path = @".\DataStorage.txt";
+            UpdateNextNumberBoard();
+        }
+
+        private void UpdateNextNumberBoard()
+        {
+            int max = 0;
+            foreach (int currentNumberBoard in Boards.Keys)
+            {
+                if (currentNumberBoard > max)
+                {
+                    max = currentNumberBoard;
+                }
+            }
+            NextNumberBoard = max + 1;
         }
     }
 }
+
