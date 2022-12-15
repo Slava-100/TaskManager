a693db93f88830baf21a32eea11295c2e27494af
malinkowork@gmail.com <malinkowork@gmail.com>
2022-12-15 13:43:21 +0300

    Added addition/removal tests Issues

--- TaskManager.Tests/BoardTests.cs
+++ TaskManager.Tests/BoardTests.cs
@@ -1,14 +1,52 @@
-using System.Text.Json;
+using static TaskManager.Tests.TestCases.BoardTestCaseSource;
 
 namespace TaskManager.Tests
 {
     public class BoardTests
     {
-        [TestCaseSource(typeof(BoardTestCaseSource), nameof(BoardTestCaseSource.GetNextNumberIssueTestSource))]
-        public void GetNextNumberIssueTest(Board board, int expectedNumberIssue)
+        private Board _board;
+
+        [SetUp]
+        public void SetUp()
         {
-            int actualNumberIssue = board.GetNextNumberIssue();
-            Assert.AreEqual(expectedNumberIssue, actualNumberIssue);
+            _board = new Board(1, "@test");
+        }
+
+        [TestCaseSource(typeof(TestCaseForAddNewIssueTest))]
+        public void AddNewIssueTest(List<Issue> issues, string description, bool exceptionResult, int exceptionNumberNewIssue)
+        {
+            _board.Issues.AddRange(issues);
+            _board.AddNewIssue(description);
+            bool actualResult = _board.Issues.Exists(issue => issue.Description == description);
+            int actualNumberNewIssue = _board.Issues[_board.Issues.Count - 1].NumberIssue;
+
+            Assert.AreEqual(exceptionResult, actualResult);
+            Assert.AreEqual(exceptionNumberNewIssue, actualNumberNewIssue);
+        }
+
+        [TestCaseSource(typeof(TestCaseForRemoveIssueTest))]
+        public void RemoveIssueTest(List<Issue> issues, int numberIssues, bool exceptionResult)
+        {
+            _board.Issues.AddRange(issues);
+            _board.RemoveIssue(numberIssues);
+            bool actualResult = _board.Issues.Exists(issues => issues.NumberIssue == numberIssues);
+
+            Assert.AreEqual(exceptionResult, actualResult);
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(3)]
+        public void RemoveIssueTest_WhenNumberIssueIncorrect_ShouldThrowArgumentOutOfRangeException(int numberIssue)
+        {
+            List<Issue> newIssue = new List<Issue>()
+            {
+                new Issue(1,"QQQ"),
+                new Issue(2,"WWW")
+            };
+            _board.Issues.AddRange(newIssue);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _board.RemoveIssue(numberIssue));
         }
     }
 }
---
+++ TaskManager.Tests/TestCases/BoardTestCaseSource.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace TaskManager.Tests.TestCases
+{
+    public static class BoardTestCaseSource
+    {
+        public class TestCaseForAddNewIssueTest : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                List<Issue> issues = new List<Issue>()
+            {
+                new Issue(1, "1"),
+                new Issue(2, "1"),
+                new Issue(3, "1"),
+                new Issue(4, "1"),
+            };
+                string description = "QQQ";
+                bool exceptionResult = true;
+                int exceptionNumberNewIssue = 5;
+
+                yield return new object[] { issues, description, exceptionResult, exceptionNumberNewIssue };
+
+                issues = new List<Issue>();
+                description = "YYY";
+                exceptionResult = true;
+                exceptionNumberNewIssue = 1;
+
+                yield return new object[] { issues, description, exceptionResult, exceptionNumberNewIssue };
+            }
+        }
+
+        public class TestCaseForRemoveIssueTest : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                List<Issue> issues = new List<Issue>()
+            {
+                new Issue(1, "1"),
+                new Issue(2, "1"),
+                new Issue(3, "1"),
+                new Issue(4, "1"),
+            };
+                int numberIssue = 4;
+                bool exceptionResult = false;
+
+                yield return new object[] { issues, numberIssue, exceptionResult };
+            }
+        }
+    }
+}
