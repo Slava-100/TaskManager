582182d2ec55f3c55c2210d4fcd26e0ea80d10cc
malinkowork@gmail.com <malinkowork@gmail.com>
2022-12-15 14:17:52 +0300

    Corrected the style code

--- TaskManager/Board.cs
+++ TaskManager/Board.cs
@@ -2,6 +2,8 @@
 {
     public class Board
     {
+        private int _numberNextIssue = 1;
+
         public int NumberBoard { get; private set; }
 
         public List<string> IDMembers { get; private set; }
@@ -11,8 +13,6 @@
         public List<Issue> Issues { get; private set; }
 
         public List<int> Keys { get; private set; }
-
-        private int _numberNextIssue = 1;
 
         public Board(int numberBoard, string idAdmin)
         {
