d30d74691955e5939bf52497c76361e68e5b8771
Kristina <bks1994@mail.ru>
2022-12-16 17:35:14 +0300

    delete duplicated file BoardTestCaseSource

--- TaskManager.Tests/BoardTests.cs
+++ TaskManager.Tests/BoardTests.cs
@@ -19,7 +19,7 @@ namespace TaskManager.Tests
             int actualNumberIssue = board.GetNextNumberIssue();
             Assert.AreEqual(expectedNumberIssue, actualNumberIssue);
         }
-        
+
         [TestCaseSource(typeof(BoardTestCaseSource), nameof(BoardTestCaseSource.AddBlokingAndBlockedByIssueTestSource))]
         public void AddBlokingAndBlockedByIssueTest(Issue blockedByCurrentIssue, Issue blockingCurrentIssue, List<int> expectedBlockedByCurrentIssue, List<int> expectedBlockingIssues)
         {
@@ -54,12 +54,6 @@ namespace TaskManager.Tests
 
             Assert.AreEqual(exceptionResult, actualResult);
         }
-//        [TestCaseSource(typeof(BoardTestCaseSource), nameof(BoardTestCaseSource.GetNextNumberIssueTestSource))]
-//        public void GetNextNumberIssueTest(Board board, int expectedNumberIssue)
-//        {
-//            int actualNumberIssue = board.GetNextNumberIssue();
-//            Assert.AreEqual(expectedNumberIssue, actualNumberIssue);
-//        }
     }
 }
 
