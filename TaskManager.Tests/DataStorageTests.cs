a258c185ab17b1d5378d3684e9a52c1d6a8cbd1c
YuraMalinko <malinkowork@gmail.com>
2022-12-18 00:28:06 +0300

    Tests for RewriteFile and ReturnFromFile methods

--- TaskManager.Tests/DataStorageTests.cs
+++ TaskManager.Tests/DataStorageTests.cs
@@ -1,5 +1,8 @@
 ﻿using FluentAssertions;
 using System;
+using NUnit.Framework;
+using System.Text.Json;
+//using Newtonsoft.Json;
 using TaskManager.Tests.TestCaseSource;
 using static TaskManager.Tests.TestCaseSource.DataStorageTestCaseSource;
 
@@ -7,11 +10,25 @@ namespace TaskManager.Tests
 {
     public class DataStorageTests
     {
-		[TestCaseSource(typeof(AddNewUserByKeyTestCaseSource))]
-		public void AddNewUserByKeyTest(string nameMember, int numberBoard, DataStorage dataStorage, string idMember, int keyBoard, List<string> expectedIdMembers, Dictionary<string, User> expectedDictionaryUsers, List<int> expectedBoardsForUser)
-		{
+        private string _path;
+
+        private DataStorage _dataStorage;
+
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            _path = @".\TestDataStorage";
+            _dataStorage = DataStorage.GetInstance();
+            _dataStorage.Path = _path;
+        }
+
+        [TestCaseSource(typeof(AddNewUserByKeyTestCaseSource))]
+        public void AddNewUserByKeyTest(string nameMember, int numberBoard, DataStorage dataStorage, string idMember, int keyBoard, List<string> expectedIdMembers, Dictionary<string, User> expectedDictionaryUsers, List<int> expectedBoardsForUser)
+        {
             bool expectedFlag = true;
-			bool actualFlag = dataStorage.AddNewUserByKey(numberBoard, keyBoard, idMember, nameMember);
+            bool actualFlag = dataStorage.AddNewUserByKey(numberBoard, keyBoard, idMember, nameMember);
 
             Assert.AreEqual(expectedFlag, actualFlag);
 
@@ -28,17 +45,17 @@ namespace TaskManager.Tests
             CollectionAssert.AreEqual(expectedDictionaryUsers, actualDictionaryUsers);
         }
 
-		[Test]
-		public void AddNewUserByKeyTest_WhenBoardDoesNotExistWithId()
-		{
-			DataStorage dataStorage = new DataStorage();
+        [Test]
+        public void AddNewUserByKeyTest_WhenBoardDoesNotExistWithId()
+        {
+            DataStorage dataStorage = new DataStorage();
             int numberBoard = 1;
             string idMember = "id";
             int keyBoard = 1;
             string nameMember = "";
 
             bool expectedFlag = false;
-            bool actualFlag = dataStorage.AddNewUserByKey(numberBoard,keyBoard,idMember,nameMember);
+            bool actualFlag = dataStorage.AddNewUserByKey(numberBoard, keyBoard, idMember, nameMember);
 
             Assert.AreEqual(expectedFlag, actualFlag);
         }
@@ -53,7 +70,7 @@ namespace TaskManager.Tests
 
             Dictionary<int, Board> storageBoards = new Dictionary<int, Board>()
             {
-                { numberBoard, new Board(numberBoard,idMember) } 
+                { numberBoard, new Board(numberBoard,idMember) }
             };
 
             storageBoards[numberBoard].Key = 2;
@@ -68,7 +85,7 @@ namespace TaskManager.Tests
         }
 
         [TestCaseSource(typeof(DataStorageTestCaseSource), nameof(DataStorageTestCaseSource.RemoveBoardTestSource))]
-        public void RemoveBoardTest(Dictionary<int, Board> baseBoards, int boardNumber, Dictionary<int,Board> expectedBoards, bool expectedBool )
+        public void RemoveBoardTest(Dictionary<int, Board> baseBoards, int boardNumber, Dictionary<int, Board> expectedBoards, bool expectedBool)
         {
             //Given
             DataStorage dataStorage = new DataStorage();
@@ -98,7 +115,51 @@ namespace TaskManager.Tests
             Assert.That(actualNumberBoard, Is.EqualTo(expectedNumberBoard));
             actualBoards.Should().BeEquivalentTo(expectedBoards);
         }
-    }
+
+        [TestCaseSource(typeof(TestCaseForRewriteAndReturnTest))]
+        public void RewriteFileTest(Dictionary<int, Board> boards, Dictionary<string, User> users)
+        {
+            _dataStorage.Boards = boards;
+            _dataStorage.Users = users;
+            _dataStorage.RewriteFile();
+            using (StreamReader sr = new StreamReader(_dataStorage.Path))
+            {
+                string jsn = sr.ReadLine();
+                _dataStorage.Boards = JsonSerializer.Deserialize<Dictionary<int, Board>>(jsn);
+                jsn = sr.ReadLine();
+                _dataStorage.Users = JsonSerializer.Deserialize<Dictionary<string, User>>(jsn);
+            }
+            Dictionary<int, Board> expectedBoards = boards;
+            Dictionary<int, Board> actualBoards = _dataStorage.Boards;
+            Dictionary<string, User> expectedUsers = users;
+            Dictionary<string, User> actualUsers = _dataStorage.Users;
+
+            CollectionAssert.AreEqual(actualBoards, expectedBoards);
+            CollectionAssert.AreEqual(actualUsers, expectedUsers);
+        }
+
+        [TestCaseSource(typeof(TestCaseForRewriteAndReturnTest))]
+        public void ReturnFromFIleTest(Dictionary<int, Board> boards, Dictionary<string, User> users)
+        {
+            _dataStorage.Boards = boards;
+            _dataStorage.Users = users;
+            using (StreamWriter sw = new StreamWriter(_dataStorage.Path))
+            {
+                string serialiseToFile = JsonSerializer.Serialize(_dataStorage.Boards);
+                sw.WriteLine(serialiseToFile);
+                serialiseToFile = JsonSerializer.Serialize(_dataStorage.Users);
+                sw.WriteLine(serialiseToFile);
+            }
+            _dataStorage.ReturnFromFile();
+            Dictionary<int, Board> expectedBoards = boards;
+            Dictionary<int, Board> actualBoards = _dataStorage.Boards;
+            Dictionary<string, User> expectedUsers = users;
+            Dictionary<string, User> actualUsers = _dataStorage.Users;
+
+            CollectionAssert.AreEqual(actualBoards, expectedBoards);
+            CollectionAssert.AreEqual(actualUsers, expectedUsers);
+        }
+    }    
 }
 
 
