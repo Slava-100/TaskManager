923b38b0765c1bf1042bdf09c76985d07de3e8e5
Svyatoslav <svat.strel.2001@gmail.com>
2022-12-17 23:05:40 +0300

    Correct

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -5,32 +5,3 @@ Console.WriteLine("Hy");
 
 TelegramService client = new TelegramService();
 Console.ReadLine();
-﻿using TaskManager;
-
-User user = new User("1", "one");
-
-Board board = new(1,"11");
-
-board.IDAdmin.Add(user.IDUser);
-
-if (user.SelectRole(board))
-{
-    user.AddNewIssue(board, "111");
-    user.AddNewIssue(board, "222");
-    var idIssue = board.Issues.First(issue => issue.Description == "222").NumberIssue;
-    user.RemoveIssue(board, idIssue);
-}
-
-//if (user.SelectRole(board))
-//{
-//    user.AddNewIssue(board, "333", out int issueId333);
-//    user.AddNewIssue(board,"444", out int issueId444);
-//    user.RemoveIssue(board, issueId444);
-
-//    user.AddNewIssue(board, "555", out int issueId555);
-//    user.AddBlokingAndBlockedByIssue(board, issueId333, issueId555);
-
-//}
-
-Console.WriteLine();
-Console.WriteLine("Hy");
