20d86d88b2763c6355395594a6f2d07e84c149e5
Kristina <bks1994@mail.ru>
2022-12-20 13:10:55 +0300

    28 - add AdminUserTest

--- TaskManager.Tests/AdminUserTest.cs
+++ TaskManager.Tests/AdminUserTest.cs
@@ -1,6 +1,42 @@
-﻿namespace TaskManager.Tests
+﻿using System.Text.Json;
+using TaskManager;
+using TaskManager.Tests.TestCaseSource;
+using FluentAssertions;
+
+
+namespace TaskManager.Tests
 {
     public class AdminUserTest
     {
+        [TestCaseSource(typeof(AdminUserTestCaseSource), nameof(AdminUserTestCaseSource.AttachIssueToClientTestCaseSource))]
+        public void AttachIssueToClientTest(List<Issue> baseIssues, Board board, Issue attachIssue, long IDUser, List<Issue> expectedIssues)
+        {
+            DataStorage.GetInstance().PathFileForClient = @"C:\Users\Кристина\Desktop\MakeUPro\Коды\Tests\AdminUserTest.txt";
+            string path = DataStorage.GetInstance().PathFileForClient;
+            DataStorage.GetInstance().Boards = new Dictionary<int, Board> { { board.NumberBoard, board } };
+            board.IDAdmin.Add(IDUser);
+            attachIssue.IdUser = IDUser;
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                string jsn = JsonSerializer.Serialize(baseIssues);
+                sw.WriteLine(jsn);
+            }
+            AdminUser adminUser = new AdminUser();
+            adminUser.AttachIssueToClient(board, attachIssue, IDUser);
+            List<Issue> actualIssues = new List<Issue>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string jsn = sr.ReadLine();
+                actualIssues = JsonSerializer.Deserialize<List<Issue>>(jsn);
+            }
+            
+            actualIssues.Should().BeEquivalentTo(expectedIssues);
+        }
     }
 }
+
+//public void AttachIssueToClient(Board board, Issue issue, long IDUser)
+//{
+//    issue.IdUser = IDUser;
+//    board.Issues.Add(issue);
+//}
--- TaskManager.Tests/TestCaseSource/AdminUserTestCaseSource.cs
+++ TaskManager.Tests/TestCaseSource/AdminUserTestCaseSource.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace TaskManager.Tests.TestCaseSource
 {
-    internal class AdminUserTestCaseSource
+    public static class AdminUserTestCaseSource
     {
+        public static IEnumerable AttachIssueToClientTestCaseSource()
+        {
+            Issue issue1 = new Issue();
+            Issue issue2 = new Issue(2, "2");
+            Issue attachIssue = new Issue(3, "3");
+            List<Issue> baseIssues = new List<Issue> { issue1, issue2, attachIssue };
+            Board board = new Board(10, 10);
+            long IDUser = 100;
+            List<Issue> expectedIssues = new List<Issue> { issue1, issue2, attachIssue };
+
+            yield return new Object[] { baseIssues, board, attachIssue, IDUser, expectedIssues };
+
+            attachIssue = new Issue(8, "8");
+            baseIssues = new List<Issue> { attachIssue };
+            expectedIssues = new List<Issue> { attachIssue };
+
+            yield return new Object[] { baseIssues, board, attachIssue, IDUser, expectedIssues };
+        }
     }
 }
+
+//(List<Issue> baseIssues, Board board, Issue attachIssue, long IDUser, List<Issue> expectedIssues)
