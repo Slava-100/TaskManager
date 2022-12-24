b5ad28539afc62f42f7297fb251d107611f2cb2a
YuraMalinko <malinkowork@gmail.com>
2022-12-24 19:48:32 +0300

    Add Methods for UserService Class

--- TaskManager/UserService.cs
+++ TaskManager/UserService.cs
@@ -1,7 +1,40 @@
-﻿namespace TaskManager
+﻿using TaskManager.Handler;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TaskManager
 {
     public class UserService
     {
+        public ITelegramBotClient TgClient { get; set; }
 
+        private IHandler _handler;
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public Client ClientUserService { get; set; }
+
+        public UserService(long id, string name, ITelegramBotClient client)
+        {
+            TgClient = client;
+            Id = id;
+            Name = name;
+            _handler = new StartHandler();
+            if (DataStorage.GetInstance().Clients.ContainsKey(id))
+            {
+                ClientUserService = DataStorage.GetInstance().Clients[id];
+            }
+        }
+
+        public void SetHandler(IHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public void HandleUpdate(Update update)
+        {
+            _handler.HandleUpdateHandler(update, this);
+        }
     }
 }
