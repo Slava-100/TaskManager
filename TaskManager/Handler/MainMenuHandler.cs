96526cf548dbc5a4beb14431ed9f9c19d489cc90
YuraMalinko <malinkowork@gmail.com>
2022-12-25 20:09:04 +0300

    Class MainMenuHandler : Corrected the code

--- TaskManager/Handler/MainMenuHandler.cs
+++ TaskManager/Handler/MainMenuHandler.cs
@@ -8,49 +8,52 @@ namespace TaskManager.Handl
 {
     public class MainMenuHandler : IHandler
     {
-        public void HandleUpdateHandler(Update update, UserService userServise)
+        public void HandleUpdateHandler(Update update, UserService userService)
         {
             switch (update.Type)
             {
+                
                 case UpdateType.CallbackQuery:
+                    ClearButtons(userService,update);
                     switch (update.CallbackQuery.Data)
                     {
                         case "AddBoard":
-                            userServise.TgClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, update.CallbackQuery.Message.Text, replyMarkup: null);
-                            userServise.SetHandler(new StartHandler());
-                            userServise.HandleUpdate(update);
+                            userService.SetHandler(new AddNewBoardHandler());
+                            userService.HandleUpdate(update);
                             break;
                         case "JoinTheBoard":
-                            userServise.TgClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, update.CallbackQuery.Message.Text, replyMarkup: null);
-                            userServise.SetHandler(new StartHandler());
-                            userServise.HandleUpdate(update);
+                            userService.SetHandler(new StartHandler());
+                            userService.HandleUpdate(update);
                             break;
                         case "WorkWithBoard":
-                            userServise.TgClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, update.CallbackQuery.Message.Text, replyMarkup: null);
-                            userServise.SetHandler(new StartHandler());
-                            userServise.HandleUpdate(update);
+                            userService.SetHandler(new StartHandler());
+                            userService.HandleUpdate(update);
                             break;
                         default:
-                            SendBaseMenu(userServise);
+                            SendBaseMenu(userService);
                             break;
                     }
                     break;
                 default:
-                    SendBaseMenu(userServise);
+                    SendBaseMenu(userService);
                     break;
             }
+        }
+
+        private void ClearButtons(UserService userService,Update update)
+        {
+            userService.TgClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, update.CallbackQuery.Message.Text, replyMarkup: null);
         }
 
         private void SendBaseMenu(UserService userServise)
         {
             InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(
-                                new[]
-                                    {
-                                    new[]{new InlineKeyboardButton("Создать новую доску") {CallbackData = "AddBoard"} },
-                                    new[]{new InlineKeyboardButton("Присоединиться к доске") {CallbackData="JoinTheBoard"}},
-                                    new[]{new InlineKeyboardButton("Работать с доской") {CallbackData="WorkWithBoard"}},
-                                });
-
+            new[]
+            {
+                 new[]{new InlineKeyboardButton("Создать новую доску") {CallbackData = "AddBoard"} },
+                 new[]{new InlineKeyboardButton("Присоединиться к доске") {CallbackData="JoinTheBoard"}},
+                 new[]{new InlineKeyboardButton("Работать с доской") {CallbackData="WorkWithBoard"}},
+            });
             userServise.TgClient.SendTextMessageAsync(userServise.Id, "Главное меню", replyMarkup: keyboard);
         }
     }
